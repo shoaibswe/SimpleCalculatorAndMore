b393d22f61ac2e9ec7ed059f788fe7c21eab75ef
NCT <NCT@Shoaib>
2017-12-12 23:44:35 +0600

    admin comment added....

---
+++ src/Calc.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shoaib
+ */
+public class Calc {
+ int reader_one,reader_two;
+ Calc(int reader_1,int reader_2){
+ reader_one=reader_1;
+ reader_two=reader_2;
+  }
+int add(){
+return reader_one+reader_two;
+}
+int sub(){
+
+return reader_one-reader_two;
+        }
+int div(){
+
+return reader_one/reader_two;
+        }
+
+}
+
